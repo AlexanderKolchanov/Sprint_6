e1c911110e9843df683a593c34ef6c49cd25e638
AlexanderKolchanov <xsandr1015@gmail.com>
2025-09-30 01:11:19 +0300

    Обновлён тест test_order_page и allur-results

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -6,36 +6,31 @@ from data import DataUser1, DataUser2
 
 
 class TestOrder:
-    """
-    Класс тестов для проверки функциональности оформления заказа.
-    """
-
-    @pytest.mark.parametrize('user_data, button_method', [
-        (DataUser1(), 'click_order_button_top'),
-        (DataUser2(), 'click_order_button_bottom')
-    ], ids=['top_button', 'bottom_button'])
-    @allure.title('Проверка успешного оформления заказа')
-    @allure.description('Тестирование позитивного сценария заказа из разных точек входа')
-    def test_successful_order(self, driver, user_data, button_method):
-        """
-        Тест проверяет успешное оформление заказа самоката.
-        
-        Args:
-            driver: Фикстура с инициализированным браузером
-            user_data: Данные пользователя для заполнения формы
-            button_method (str): Название метода для клика по кнопке заказа
-        """
+    
+    @allure.title('Проверка успешного оформления заказа через верхнюю кнопку')
+    def test_successful_order_via_top_button(self, driver):
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
         
         main_page.open()
         main_page.accept_cookies()
+        main_page.click_order_button_top()  # ✅ Конкретный метод
         
-        # ✅ ИСПРАВЛЕНО: убрано условие if/else, используем getattr
-        click_method = getattr(main_page, button_method)
-        click_method()  # Вызываем метод клика по кнопке
+        order_page.data_entry_first_form(DataUser1())
+        order_page.data_entry_second_form(DataUser1())
         
-        order_page.data_entry_first_form(user_data)
-        order_page.data_entry_second_form(user_data)
+        assert order_page.check_displaying_of_button_check_status_of_order()
+
+    @allure.title('Проверка успешного оформления заказа через нижнюю кнопку')
+    def test_successful_order_via_bottom_button(self, driver):
+        main_page = MainPage(driver)
+        order_page = OrderPage(driver)
+        
+        main_page.open()
+        main_page.accept_cookies()
+        main_page.click_order_button_bottom()  # ✅ Конкретный метод
+        
+        order_page.data_entry_first_form(DataUser2())
+        order_page.data_entry_second_form(DataUser2())
         
         assert order_page.check_displaying_of_button_check_status_of_order()
